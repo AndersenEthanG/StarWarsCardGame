c5bc733024b877e521cb65783c9afbfde6a4bff3
Ethan Andersen <Ethan@Ethans-iMac.local>
2021-04-22 20:39:24 -0600

    Turned from some random files into a working app

---
+++ StarWarsCardGame/StarWarsCardGame/Models/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  StarWarsCardGame
+//
+//  Created by Ethan Andersen on 4/22/21.
+//
+
+import Foundation
---
+++ StarWarsCardGame/StarWarsCardGame/Views/Custom Cell/CharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterCollectionViewCell.swift
+//  StarWarsCardGame
+//
+//  Created by Ethan Andersen on 4/22/21.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+}
